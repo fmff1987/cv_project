e4b11d8d7d3d9b154cc192e9ce904ba716829c52
Ricardo Russo <russoricardo1@gmail.com>
2018-07-13 16:10:39 +0100

    ControllerRecruter
    controlador do recrutador

--- cv_project/src/main/java/cv_project/control/ControllerRecruiter.java
+++ cv_project/src/main/java/cv_project/control/ControllerRecruiter.java
@@ -1,21 +1,32 @@
-package cv_project.control;
+package control;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 import cv_project.models.Recruiter;
+import cv_project.repositories.EntityRepository;
 
 @RequestScoped
 public class ControllerRecruiter {
-	//static PessoaRepositor
+	
+	@Inject
+	EntityRepository <Recruiter> db;
 	
 	
 	
 	
 	public void createRecruiter(Recruiter p) {
-		
+		db.createEntity(p);
 	}
 	
 	public void removeRecruter(Recruiter p) {
+		db.removeEntity(p);
+		updateList();
 		
 	}
+	
+	public void updateList() {
+		db.updateLocalList();
+	}
+	
 }
