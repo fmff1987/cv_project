0315de6757584e45a57719499d3196b026e20af7
forjaz <lacerdaforjaz@gmail.com>
2018-07-13 14:55:08 +0100

    Beans & Repository
    Criei ManagerBean
    Criei RecruiterBean
    Criei GeneralRepository

---
+++ cv_project/src/main/java/beans/Managerbean.java
@@ -0,0 +1,35 @@
+package beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import cv_project.models.Manager;
+
+@RequestScoped
+public class Managerbean {
+	
+	//////////////////////////////ATRIBUTS
+	private Manager manager = new Manager();
+
+	@Inject
+	private ControllerManager cm;
+	
+	//////////////////////////////GETTER & SETTER
+	public Manager getManager() {
+		return manager;
+	}
+
+	public void setManager(Manager manager) {
+		this.manager = manager;
+	}
+
+	public ControllerManager getCm() {
+		return cm;
+	}
+
+	public void setCm(ControllerManager cm) {
+		this.cm = cm;
+	}
+	
+	
+	
+}
---
+++ cv_project/src/main/java/repositories/GeneralRepository.java
@@ -0,0 +1,41 @@
+package repositories;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import cv_project.models.Person;
+
+@Transactional
+public abstract class GeneralRepository<T extends Person> {
+
+	@PersistenceContext(unitName = "database")
+	protected EntityManager em;
+
+	protected List<T> localList;
+
+	public void createEntity(T ent) {
+		em.persist(ent);
+		updateLocalList();
+	}
+
+	public List<T> listEntity(Class<T> entClass) {
+		return localList;
+	}
+
+	public T getEntity(Class<T> entClass, Long id) {
+		return em.find(entClass, id);
+	}
+
+	public void updateEntity(T ent) {
+		em.merge(ent);
+		updateLocalList();
+	}
+
+	public void removeEntity(T ent) {
+		em.remove(em.merge(ent));
+	}
+
+	public abstract void updateLocalList();
+
+}
