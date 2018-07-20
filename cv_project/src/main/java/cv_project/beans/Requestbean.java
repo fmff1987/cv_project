e19ea41a63afa376fcdf1835fcd0c2d6ae0694b5
fmff1987 <fmff1987@users.noreply.github.com>
2018-07-20 10:24:32 +0100

    Classe Candidate
    Removidos todos os arquivos da classe candidate

--- cv_project/src/main/java/cv_project/beans/Requestbean.java
+++ cv_project/src/main/java/cv_project/beans/Requestbean.java
@@ -5,13 +5,11 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import cv_project.control.ControllerCandidate;
+
 import cv_project.control.ControllerRequest;
-import cv_project.models.Candidate;
+
 import cv_project.models.Request;
-import java.util.Date;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
 
 @Named("ReqBean")
 @RequestScoped
@@ -20,43 +18,42 @@ public class Requestbean {
    
     
 	
-        private Request request = new Request();
-	 
-	 @Inject
-	 private ControllerRequest cr;
-	 
-            
-	
-	
-	 	public Request getRequest() {
-			return request;
-		}
+private Request request = new Request();
 
-		public void setRequest(Request request) {
-			this.request = request;
-		}
+    @Inject
+    private ControllerRequest cr;
 
-		public ControllerRequest getCr() {
-			return cr;
-		}
 
-		public void setCr(ControllerRequest cr) {
-			this.cr = cr;
-		}
+    public Request getRequest() {
+            return request;
+    }
 
-		public List<Request> getReq(){
-			return cr.getReq();
-		}
+    public void setRequest(Request request) {
+            this.request = request;
+    }
 
-		public void createReq() {
-			cr.createRequest(request);
-		}
-	
-		public void removeReq() {
-			cr.removeRequest(request);
-		}
-		
-		public void updateReq() {
-			cr.updateReq();
-		}
+    public ControllerRequest getCr() {
+            return cr;
+    }
+
+    public void setCr(ControllerRequest cr) {
+            this.cr = cr;
+    }
+
+    public List<Request> getReq(){
+            return cr.getReq();
+    }
+
+    public void createReq() {
+            cr.createRequest(request);
+    }
+
+    public void removeReq() {
+            cr.removeRequest(request);
+    }
+
+    public void updateReq() {
+            cr.updateReq();
+    }
+    
 }
